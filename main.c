e48f0d2d970a91583e2d065e17ade41917e654b7
Fisiekk <fisiekkuweta@gmail.com>
2024-04-12 21:55:46 +0200

    more coodeee

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 
 /*
+ * SI5351 addr = 0x60
  * 24C64 has 16bit addresing via 2 addres bytes i2c
  * but 24C04, 24C08, 24C16 uses b3-b1 in addres as A10-A8
  *
@@ -16,7 +17,7 @@
 
 void clock_setup(void)
 {
-  rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_PLL_64MHZ])
+  rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_PLL_64MHZ]);
   
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_GPIOB);
@@ -28,16 +29,21 @@ void clock_setup(void)
 
 void gpio_setup(void)
 {
-  gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO4);
+  gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO4);
 
   /*
    * I2C2
    * PA11 = SCL; PA12 = SDA
    */
-  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO11 | GPIO12);
-  gpio_set_af(GPIOa, GPIO_AF6, GPIO11 | GPIO12);
 
-  i2c_set_speed(I2C2, i2c_speed_fm_400k, 64);
+  i2c_peripheral_disable(I2C2);
+
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO11 | GPIO12);
+  gpio_set_af(GPIOA, GPIO_AF6, GPIO11 | GPIO12);
+
+  i2c_set_speed(I2C2, i2c_speed_sm_100k, (rcc_apb1_frequency / 1000000));
+
+  i2c_peripheral_enable(I2C2);
 }
 
 int main(void)
@@ -45,9 +51,42 @@ int main(void)
   clock_setup();
   gpio_setup();
   
-  //uint8_t page_buffer[32] = {0};
+  uint8_t buffer[32] = {0};
+  uint8_t command_buffer[2] = {0};
+
+  for(uint32_t i = 0; i <= 10000000; i++)
+    __asm__("NOP");
+
+  for(uint16_t addr = 0; addr <= 127; addr++)
+    {
+      i2c_set_7bit_address(I2C2, addr);
+      i2c_set_read_transfer_dir(I2C2);
+      i2c_set_bytes_to_transfer(I2C2, 0);
+      i2c_send_start(I2C2);
+      //i2c_enable_autoend(I2C2);
+
+      while (i2c_received_data(I2C2) == 0)
+	{
+	  if(i2c_nack(I2C2))
+	    {
+	      break;
+	    }
+	  else
+	    {
+	      for(uint32_t j = 0; j <= 6400000; j++)
+		__asm__("NOP");
+	    }
+	}
+      i2c_get_data(I2C2);
+      i2c_send_stop(I2C2);
+
+      for(uint32_t i = 0; i <= 64000; i++)
+	__asm__("NOP");
+	}
   
-  gpio_set(GPIOA, GPIO4);
+  i2c_transfer7(I2C2, 0x50, 0, 0, buffer, 32);
+  
+  gpio_clear(GPIOA, GPIO4);
   
   return 0;
 }
